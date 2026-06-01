b46f68c270c1adc8a4751dbe3e0ec0fcb7a96be9
NIN12 <amouzouninon05@gmail.com>
2026-06-01 23:39:04 +0000

    fix: rename NotFound to NotFoundPage, improve 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFound: React.FC = () => {
+const NotFoundPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] text-center p-6">
       <h1 className="text-6xl font-extrabold text-[var(--accent)] mb-4">404</h1>
@@ -18,4 +18,4 @@ const NotFound: React.FC = () => {
   );
 };
 
-export default NotFound;
+export default NotFoundPage;
